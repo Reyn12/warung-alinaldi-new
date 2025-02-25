d92c73785842d782031e596a2df1bbdb0b5882e1
Reyn12 <muhammadrenaldi687@gmail.com>
2025-02-25 13:10:29 +0700

    testing error eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = [
     extends: ['next'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@next/next/no-img-element': 'off',
       'react/no-unescaped-entities': 'off',
