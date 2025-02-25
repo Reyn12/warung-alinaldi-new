d719b369d4f51e75060faed24f5b326f9fac2726
Reyn12 <muhammadrenaldi687@gmail.com>
2025-02-25 12:51:37 +0700

    Push main

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -29,6 +29,8 @@ const DialogOverlay = React.forwardRef<
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
+// Test
+
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
